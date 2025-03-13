d5b0d05ebc17bfe8b794ba80d5b07bb8f22843f8
mahruf60 <mahruf9060@gmail.com>
2025-03-13 12:17:26 +0600

    User authentication

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,21 +4,19 @@ from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password,**extra_fields):
+    def create_user(self,  email, password,**extra_fields):
 
-        if not username:
-            raise ValueError("Users must have an username")
         if not email:
             raise ValueError("Users must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(username=username,email=email,**extra_fields)
+        user = self.model(email=email,**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,username, email, password,**extra_fields):
-        user = self.create_user(username=username,email=email, password=password,**extra_fields)
+    def create_superuser(self,email, password,**extra_fields):
+        user = self.create_user(email=email, password=password,**extra_fields)
       
         user.is_staff = True
         user.is_superuser = True
@@ -54,8 +52,8 @@ class User(AbstractBaseUser):
     def __str__(self):
         return str(self.id)
     
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ['email',]
+    USERNAME_FIELD = "email"
+    
     class Meta:
         ordering = ['-date_joined']
 
