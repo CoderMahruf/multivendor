fc57e6393a7950e7fac0f9a568f3bab9384028f1
mahruf60 <mahruf9060@gmail.com>
2025-03-13 10:41:06 +0600

    Set

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,13 +1,41 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password,**extra_fields):
+
+        if not username:
+            raise ValueError("Users must have an username")
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username,email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,username, email, password,**extra_fields):
+        user = self.create_user(username=username,email=email, password=password,**extra_fields)
+      
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser):
     VENDOR = 1
     CUSTOMER = 2
+    ADMIN = 3
+    SUPERUSER = 4
     ROLE_CHOICE = (
         (VENDOR, 'Vendor'),
-        (CUSTOMER, 'Customer')
+        (CUSTOMER, 'Customer'),
+        (ADMIN, 'Admin'),
+        (SUPERUSER, 'Superuser'),
     )
     first_name = models.CharField(max_length=50,blank=True,null=True)
     last_name = models.CharField(max_length=50,blank=True,null=True)
@@ -20,14 +48,36 @@ class User(AbstractBaseUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    objects = UserManager()
     
     def __str__(self):
         return str(self.id)
     
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ['email',]
+    class Meta:
+        ordering = ['-date_joined']
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+        
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = 4
+        elif self.is_admin:
+            self.role = 3
+        super().save(*args, **kwargs)
+
 
 class UserProfile(AbstractBaseUser):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
