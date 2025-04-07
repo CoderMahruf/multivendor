693fce5735924a00e931a78e4cda5e8222f6e704
mahruf60 <mahruf9060@gmail.com>
2025-04-07 12:28:32 +0600

    Customer & Vendor registration

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,10 @@ from .views import *
 urlpatterns = [
     path('registerUser/',registerUser,name='registerUser'),
     path('registerVendor/',registerVendor,name='registerVendor'),
+
+    path('login/',login,name='login'),
+    path('logout/',logout,name='logout'),
+    path('myAccount/',myAccount,name='myAccount'),
+    path('custDashboard/',custDashboard,name='custDashboard'),
+    path('vendorDashboard/',vendorDashboard,name='vendorDashboard'),
 ]
