01dadfd4d287a7ab86651d61b9595485f2a7d00b
mahruf60 <mahruf9060@gmail.com>
2025-04-14 22:24:19 +0600

    Cart functionalities with AJAX request

--- apps/marketplace/urls.py
+++ apps/marketplace/urls.py
@@ -3,4 +3,9 @@ from .views import *
 urlpatterns = [
     path('',marketplace,name='marketplace'),
     path('<slug:vendor_slug>/',vendor_detail,name='vendor_detail'),
+
+    # Add to Cart
+    path('add_to_cart/<int:food_id>/',add_to_cart,name='add_to_cart'),
+    # DECREASE CART
+    path('decrease_cart/<int:food_id>/',decrease_cart,name='decrease_cart'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.accounts.context_processors.get_vendor',
+                'apps.marketplace.context_processors.get_cart_counter',
             ],
         },
     },
