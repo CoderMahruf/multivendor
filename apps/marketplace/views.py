1be8d3c8a9932c64e01d7ae05ae88c34be4ebad4
mahruf60 <mahruf9060@gmail.com>
2025-04-16 13:16:09 +0600

    Search Restaurent & Fooditems

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse,JsonResponse
 from django.db.models import Prefetch
 from apps.vendor.models import Vendor
@@ -108,3 +109,16 @@ def delete_cart(request,cart_id):
                 return JsonResponse({'status':'Failed','message':'Cart Item does not exists!'})
         else:
             return JsonResponse({'status':'Failed','message':'Invalid request'})
+        
+
+def search(request):
+    keyword = request.GET['keyword']
+    # get vendor ids that has the food item the user is looking for 
+    fetch_vendors_by_fooditems = FoodItem.objects.filter(food_title__icontains=keyword,is_available=True).values_list('vendor',flat=True)
+    vendors = Vendor.objects.filter(Q(id__in=fetch_vendors_by_fooditems) | Q(vendor_name__icontains=keyword, is_approved=True,user__is_active=True))
+    vendor_count = vendors.count()
+    context = {
+        'vendors':vendors,
+        'vendor_count':vendor_count,
+    }
+    return render(request,'marketplace/listings.html',context)
