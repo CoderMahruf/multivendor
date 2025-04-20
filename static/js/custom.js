135a6d5c2f3156d26394069612850998126e5a48
mahruf60 <mahruf9060@gmail.com>
2025-04-20 15:30:10 +0600

    Place order & generate order number

--- static/js/custom.js
+++ static/js/custom.js
@@ -22,6 +22,14 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+
+                    // subtotal, tax and grand total 
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax'],
+                        response.cart_amount['grand_total'],
+                    )
+
                 }
             }
         })
@@ -56,6 +64,11 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     $('#qty-'+food_id).html(response.qty);
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax_dict'],
+                        response.cart_amount['grand_total'],
+                    )
 
                     if(window.location.pathname == '/cart/'){
                         removeCartItem(response.qty,cart_id);
@@ -84,6 +97,11 @@ $(document).ready(function(){
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     swal(response.status,response.message,"success")
+                    applyCartAmounts(
+                        response.cart_amount['subtotal'],
+                        response.cart_amount['tax_dict'],
+                        response.cart_amount['grand_total'],
+                    )
                     removeCartItem(0,cart_id);
                     checkEmptyCart();
                 }
@@ -107,4 +125,85 @@ $(document).ready(function(){
             document.getElementById("empty-cart").style.display = "block";
         }
     }
+
+    // apply cart amount 
+    function applyCartAmounts(subtotal,tax_dict,grand_total){
+        if(window.location.pathname == '/cart/'){
+            $('#subtotal').html(subtotal)
+            $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+            }
+        }
+    }
+
+    // ADD OPENING HOURS
+    $('.add_hour').on('click',function(e){
+        e.preventDefault();
+        var day = document.getElementById('id_day').value
+        var from_hour = document.getElementById('id_from_hour').value
+        var to_hour = document.getElementById('id_to_hour').value
+        var is_closed = document.getElementById('id_is_closed').checked
+        var csrf_token = $('input[name=csrfmiddlewaretoken]').val()
+        var url = document.getElementById('add_hour_url').value
+        console.log(day, from_hour, to_hour, is_closed, csrf_token)
+
+        if(is_closed){
+            is_closed = 'True'
+            condition = "day != ''"
+        }else{
+            is_closed = 'False'
+            condition = "day != '' && from_hour != '' && to_hour != ''"
+        }
+        
+        if(eval(condition)){
+           $.ajax({
+            type:'POST',
+            url:url,
+            data: {
+                'day':day,
+                'from_hour':from_hour,
+                'to_hour':to_hour,
+                'is_closed':is_closed,
+                'csrfmiddlewaretoken':csrf_token,
+            },
+            success:function(response){
+                if (response.status == 'success'){
+                    if(response.is_closed == 'Closed'){
+                        html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>Closed</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+                    }else{
+                        html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>'+response.from_hour+' - '+response.to_hour+'</td><td><a href="#" class="remove_hour" data-url="/vendor/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+                    }
+                    
+                    $(".opening_hours").append(html)
+                    document.getElementById("opening_hours").reset();
+                }else{
+                    swal(response.message, '', 'error')
+                }
+            }
+           })
+        }else{
+            swal('Please fill all fields','','info')
+        }
+    })
+    // REMOVE OPENING HOUR
+    $(document).on('click','.remove_hour',function(e){
+        e.preventDefault();
+        url = $(this).attr('data-url');
+
+        $.ajax({
+            type:'GET',
+            url:url,
+            success:function(response){
+                if (response.status == 'success'){
+                    document.getElementById('hour-'+response.id).remove()
+                }
+            }
+        })
+    })
+    // document ready closed
 });
